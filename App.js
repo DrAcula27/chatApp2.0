cec14aecdd847eb634445d8f36c7ae5993834b74
DrAcula27 <danielle@kendallspace.net>
2023-07-13 11:04:09 -0600

    add stream chat to app

--- App.js
+++ App.js
@@ -5,12 +5,19 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Text } from "react-native";
+import { useChatClient } from "./useChatClient";
 
 const Stack = createStackNavigator();
 
 const HomeScreen = () => <Text>Home Screen</Text>;
 
 const NavigationStack = () => {
+  const { clientIsReady } = useChatClient();
+
+  if (!clientIsReady) {
+    return <Text>Loading chat ...</Text>;
+  }
+
   return (
     <Stack.Navigator>
       <Stack.Screen name="Home" component={HomeScreen} />
---
+++ chatConfig.js
@@ -0,0 +1,7 @@
+export const chatApiKey = "vnfrejsn6scx";
+
+// when integrating, store user info in local storage to fetch from backend
+export const chatUserId = "ancient-hall-3";
+export const chatUserToken =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiYW5jaWVudC1oYWxsLTMiLCJleHAiOjE2ODkyNzg1OTd9.G7xdg8EmT5PvGzKnjyGXCnxRfoxkGzgCXtXKlBYqelo";
+export const chatUserName = "ancient";

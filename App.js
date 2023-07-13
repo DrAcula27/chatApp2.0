2b00ec66dc2cd400a9dbc6a62d7112e4e0db8a01
DrAcula27 <danielle@kendallspace.net>
2023-07-13 10:35:36 -0600

    setup basic navigation stack

--- App.js
+++ App.js
@@ -1,29 +1,31 @@
 // gesture-handler must be imported at the top of the entry file (app.js in this case) before anything else
-import "react-native-gesture-handler";
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
-import { OverlayProvider } from "stream-chat-expo";
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Text } from "react-native";
 
-export default function App() {
+const Stack = createStackNavigator();
+
+const HomeScreen = () => <Text>Home Screen</Text>;
+
+const NavigationStack = () => {
   return (
-    // entry point must be wrapped with the <GestureHandlerRootView> component
-    <GestureHandlerRootView>
-      <OverlayProvider>
-        <View style={styles.container}>
-          <Text>Open up App.js to start working on your app!</Text>
-          <StatusBar style="auto" />
-        </View>
-      </OverlayProvider>
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} />
+    </Stack.Navigator>
+  );
+};
+
+export default () => {
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <SafeAreaView style={{ flex: 1 }}>
+        <NavigationContainer>
+          <NavigationStack />
+        </NavigationContainer>
+      </SafeAreaView>
     </GestureHandlerRootView>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+};
